b60d76f9959f921b7c40b7469ed6bb63e708aff6
Kashaf <kashafqureshii20@gmail.com>
2025-06-24 14:05:02 +0500

    Added Day 1 - Variable and Typecasting

---
+++ task 1 (internship).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+print("---< Movie Ticket Info >---\n")
+movie_name=input("- Enter the movie you are watching: ")
+
+ticket_amount=input("- Enter the number of tickets you bought: ")
+
+price=input("- Enter the price of one ticket: ")
+
+print("\n--> You bought "+ str(ticket_amount)+' tickets for the movie "'+movie_name+'" for Rs.'+str(price)+" each.")
+
+
+
+# In[ ]:
+
+
+
+
