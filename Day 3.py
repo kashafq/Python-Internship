b1720f3182120721c39e2d689e53e78def7d6915
Kashaf <kashafqureshii20@gmail.com>
2025-06-26 13:39:52 +0500

    Added Day 3 Task - Spliting , joining & tuple

---
+++ Day 3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[37]:
+
+
+print('---< DAY 3 >---\n\n')
+print("--< List >--")
+sentence=input("Please enter a sentence: ")
+print(f'Your sentence is: {sentence.capitalize()}')
+list=sentence.split()
+print (my_list)
+print(f'Your sentence as a list: {'-'.join(my_list)}')
+
+name_tuple=('Kasahaf','Qureshi')
+print('\n--< Tuple >--')
+print(f'First Name: {name_tuple[0]}')
+print(f'Last Name: {name_tuple[1]}')
+
+
+# In[ ]:
+
+
+
+
